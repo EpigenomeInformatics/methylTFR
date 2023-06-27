aec38c607e5cb3c3fab809f824dc29903afc5398
Irem Gündüz <irembgunduz@gmail.com>
2023-06-27 10:57:58 +0200

    refactoring

--- R/compute_deviations.R
+++ R/compute_deviations.R
@@ -1,134 +1,131 @@
 #' read_methylome
-#' 
-#'  read_methylome is a function to import methylation data into R Genomic 
-#'  Range object. Bed file should be processed in the pipeline developed by 
+#'
+#'  read_methylome is a function to import methylation data into R Genomic
+#'  Range object. Bed file should be processed in the pipeline developed by
 #'  Fabian Müller and Christoph Bock. (EPP format)
-#' @param  - bedfilename which contains methylation data EPP format 
+#' @param  - bedfilename which contains methylation data EPP format
 #' @return \code{GenomicRange} object with methylation, coverage information
-#' @export 
+#' @export
 #' @importFrom GenomicRanges GRanges
 #' @importFrom IRanges IRanges
 #' @importFrom data.table fread
 #' @importFrom stringr str_split str_replace
-read_methylome <- function(filename, type = c("EPP", "BisSNP") ){
-    if (!file.exists(filename)) {
-        stop(paste(filename, " doesn't exist or path is incorrect !!"))
-    }
-    type = match.arg(type)
-    if (type == "EPP") {
-        # Parse EPP file format
-        msites <- fread(filename, header = FALSE)
-        mcov <- unlist(str_split(msites$V4, "/"))
-        mcov <- as.numeric(str_replace(mcov, "'", ""))
-        row_odd <- seq_len(length(mcov)) %% 2 
-        cov  <- mcov[row_odd == 0]
-        mscore <- round(msites$V5/1000, 3)
-    } else {
-        # Parse BisSNP Tab-Separated file format 
-        msites <- fread(filename, header = FALSE, skip = 1)
-        mscore <- round(msites$V4/100, 3)
-        cov <- msites$V5
-    }
+read_methylome <- function(filename, type = c("EPP", "BisSNP")) {
+  if (!file.exists(filename)) {
+    stop(paste(filename, " doesn't exist or path is incorrect !!"))
+  }
+  type <- match.arg(type)
+  if (type == "EPP") {
+    # Parse EPP file format
+    msites <- fread(filename, header = FALSE, showProgress = FALSE)
+    mcov <- unlist(str_split(msites$V4, "/"))
+    mcov <- as.numeric(str_replace(mcov, "'", ""))
+    cov <- mcov[seq_along(mcov) %% 2 == 0]
+    mscore <- round(msites$V5 / 1000, 3)
+  } else {
+    # Parse BisSNP Tab-Separated file format
+    msites <- fread(filename, header = FALSE, skip = 1, showProgress = FALSE)
+    mscore <- round(msites$V4 / 100, 3)
+    cov <- msites$V5
+  }
 
-    msites <- GRanges(seqnames = msites$V1, 
-                        ranges = IRanges(start = msites$V2, width = 3), 
-                        strand = msites$V6,
-                        score = mscore,
-                        methylation = msites$V4,
-                        coverage = cov)
+  msites <- GRanges(
+    seqnames = msites$V1,
+    ranges = IRanges(start = msites$V2, end = msites$V2 + 2),
+    strand = msites$V6,
+    score = mscore,
+    methylation = msites$V4,
+    coverage = cov
+  )
 
-    return(msites)
+  return(msites)
 }
 
 
 #' calculate_expmeth
-#' 
-#'      This function is used to calculate genome-wide expected methylation 
-#'  for each motif. 
 #'
-#' @param msites - imported methylation sites 
+#'      This function is used to calculate genome-wide expected methylation
+#'  for each motif.
+#'
+#' @param msites - imported methylation sites
 #' @param gcdist - Genome wide GC distribution from (\code{methylTFRann})
 #' @param gcfreq - GC bin frequency table (matrix) from (\code{methylTFRann})
 #' @return a \code{data.table} object with GC bin with corresponding avg methylation
-#' @export 
+#' @export
 #' @importFrom GenomicRanges GRanges findOverlaps
 #' @importFrom data.table data.table
-#' @importFrom dplyr %>%
-calculate_expmeth <- function(msites, gcdist, gcfreq){
-    hits <- findOverlaps(msites, gcdist, type = "within")
-    gcmap <- data.table(mscore = msites[hits@from]$score, 
-                        gcbin = gcdist[hits@to]$GC_bin)
-    exp_meth <- gcmap %>% 
-                group_by(gcbin) %>% 
-                summarise(avg_mscore = mean(mscore))
-    exp_meth <- as.matrix(exp_meth %>% 
-                          group_by(gcbin) %>% 
-                          summarise(mscore = mean(avg_mscore)))
-    exp.data <- t(gcfreq) %*% exp_meth[, 2]
-    mpos <- seq(-floor(length(exp.data)/2), floor(length(exp.data)/2))
-    exp.methyl <-  data.table(x = mpos, avg_methyl = exp.data)
-    colnames(exp.methyl) <- c("x", "exp_avg_methyl")
-    return(exp.methyl)
+calculate_expmeth <- function(msites, gcdist, gcfreq) {
+  hits <- findOverlaps(msites, gcdist, type = "within")
+  gcmap <- data.table(
+    mscore = msites[hits@from]$score,
+    gcbin = gcdist[hits@to]$GC_bin
+  )
+  exp_meth <- gcmap[, .(avg_mscore = mean(mscore)), by = gcbin]
+  exp_meth <- exp_meth[, .(mscore = mean(avg_mscore)), by = gcbin]
+  exp_meth <- as.matrix(exp_meth)
+  exp.data <- t(gcfreq) %*% exp_meth[, 2]
+  mpos <- seq(-floor(length(exp.data) / 2), floor(length(exp.data) / 2))
+  exp.methyl <- data.table(x = mpos, avg_methyl = exp.data)
+  colnames(exp.methyl) <- c("x", "exp_avg_methyl")
+  return(exp.methyl)
 }
 
 
 #' compute_deviation
-#' 
+#'
 #'      This function is function to calculate the deviation in transcription factor
 #'  footprint base for a given motif
 #'
-#' @param motifs        - list of motifs 
+#' @param motifs        - list of motifs
 #' @param msites       - imported methylation sites
 #' @param tf_bindsites - a GenomicRange object contains tf binding sites positions from (\code{methylTFRann})
 #' @param gcfreqs      - GC bin frequency tables (matrices for multiple motif) from (\code{methylTFRann})
 #' @param gcdist       - Genome wide GC distribution from (\code{methylTFRann})
 #' @param enhancer     - Specific regions like distal motif
 #' @return deviation score for a given motif
-#' @export 
+#' @export
 #' @importFrom GenomicRanges GRanges findOverlaps width resize start end mcols
-#' @importFrom data.table data.table
-#' @importFrom dplyr %>% summarise group_by n
+#' @importFrom data.table data.table setDT
+#' @importFrom dplyr n left_join
 compute_deviation <- function(motif, msites, tf_bindsites, gcfreqs,
-                                gcdist, enhancer = NULL){
-    tfbs <- tf_bindsites[[motif]]
-    gcfreq <- gcfreqs[[motif]]
-    w <- width(tfbs)[1]
-    # This method is to increase the width as 500 bp around motif center
-    tfbs <- resize(tfbs, w + 101, fix = "center")
+                              gcdist, enhancer = NULL) {
+  tfbs <- tf_bindsites[[motif]]
+  gcfreq <- gcfreqs[[motif]]
+  w <- width(tfbs)[1]
+  tfbs <- resize(tfbs, w + 101, fix = "center")
 
-    if (!is.null(enhancer)){
-        d_hits <- findOverlaps(tfbs, enhancer)
-        tfbs <- tfbs[d_hits@from]
-    }
-    # expected methylation
-    exp_meth <- calculate_expmeth(msites, gcdist, gcfreq)
+  if (!is.null(enhancer)) {
+    d_hits <- findOverlaps(tfbs, enhancer)
+    tfbs <- tfbs[d_hits@from]
+  }
 
-    hits <- findOverlaps(msites, tfbs, type = "within")
+  exp_meth <- calculate_expmeth(msites, gcdist, gcfreq)
+  hits <- findOverlaps(msites, tfbs, type = "within")
 
-    mcols(tfbs)$mid_point <- round(end(tfbs) + ((start(tfbs) - end(tfbs))/2))
-    x = start(msites[hits@from]) - tfbs[hits@to]$mid_point
-    plot.data <- data.table(x = x, 
-                            y1 = (msites[hits@from]$score), 
-                            y2 = msites[hits@from]$coverage)
+  mcols(tfbs)$mid_point <- round(end(tfbs) + ((start(tfbs) - end(tfbs)) / 2))
+  x <- start(msites[hits@from]) - tfbs[hits@to]$mid_point
+  plot.data <- data.table::data.table(
+    x = x,
+    y1 = msites[hits@from]$score,
+    y2 = msites[hits@from]$coverage
+  )
 
-    plot.data <- plot.data %>% group_by(x) %>% 
-                    summarise(n=n(), avg_methyl=mean(y1), avg_cov = mean(y2),  
-                              motif=motif)
-    # GC bias correcton 
-    dt_join <- left_join(plot.data, exp_meth, by='x')
-    dt_join$diff <- abs(dt_join$avg_methyl - dt_join$exp_avg_methyl)
+  plot.data <- plot.data[, .(n = .N, avg_methyl = mean(y1), avg_cov = mean(y2), motif = motif), by = x]
+  dt_join <- dplyr::left_join(plot.data, exp_meth, by = "x")
+  data.table::setDT(dt_join)
+  dt_join[, diff := abs(avg_methyl - exp_avg_methyl)]
 
-    interval_mean <- dt_join %>% mutate(cuts = cut(x, c(-200, -100, -10, 10, 100, 200))) %>% 
-                        group_by(cuts) %>% 
-                        summarize(n=n(), mean = mean(diff))
-    
-    var <- interval_mean$mean[3]/((interval_mean$mean[1] + interval_mean$mean[5])/2)
-    return(var)  
+  dt_join[, cuts := cut(x, c(-200, -100, -10, 10, 100, 200))]
+  interval_mean <- dt_join[, .(n = .N, mean = mean(diff)), by = cuts]
+
+  var <- interval_mean$mean[3] / ((interval_mean$mean[1] + interval_mean$mean[5]) / 2)
+  return(var)
 }
 
 
+
 #' run_methyltfr
-#' 
+#'
 #'      This function is a wrapper function to calculate the deviation in transcription factor
 #'  footprint base for all given motifs using parallel package
 #'
@@ -137,54 +134,64 @@ compute_deviation <- function(motif, msites, tf_bindsites, gcfreqs,
 #' @param genome       - human genome version default: hg38
 #' @param threads      - thread count for parallel processing
 #' @param enhancer     - Specific regions like distal motif
-#' @return deviation score matrix for all samples 
-#' @export 
+#' @return deviation score matrix for all samples
+#' @export
 #' @importFrom GenomicRanges GRanges findOverlaps width resize start end
 #' @importFrom data.table data.table
 #' @importFrom dplyr %>%
 #' @importFrom parallel mclapply
-#' @importFrom logger log_info log_error
-run_methyltfr <- function(sample_ann, sample_dir, genome="hg38",
-                            threads = 4, enhancer = NULL, filetype = c("EPP", "BisSNP")){
-    
-    if (!require("methylTFRann")) {
-        log_error("methylTFRann package is not installed in your environment !!")
-    }
+#' @importFrom logger log_info log_error log_appender appender_file
+run_methyltfr <- function(sample_ann, sample_dir, genome = "hg38",
+                          threads = 4, enhancer = NULL, filetype = c("EPP", "BisSNP")) {
+  # Set the log file name with current date
+  log_file <- file.path(sample_dir, paste0("methyltfr_run_", format(Sys.Date(), "%Y%m%d"), ".log"))
 
-    annfile = file.path(sample_dir, sample_ann)
-    if (!file.exists(annfile)){
-        log_error(" %s does not exist, please check the file path !!", annfile)
-    }
+  # Create the log file if it doesn't exist
+  if (!file.exists(log_file)) {
+    file.create(log_file)
+  }
 
-    samples <- read.table(annfile, sep = "\t", header=TRUE)
-    filetype  <- match.arg(filetype)
+  # Initialize the logger
+  logger::log_appender(logger::appender_file(log_file))
 
-    log_info("Loading the samples and annotation package ")
-    files_list <- file.path(sample_dir, samples[, "bedFile"])
-    tf_bindsites <- getTFbindsites()
-    gc_dist <- getGenomeGC()
-    gcfreqs <- getGCfreq()
-    
+  if (!require("methylTFRann")) {
+    logger::log_error("methylTFRann package is not installed in your environment !!")
+  }
 
-    motifs <- names(gcfreqs)
-    deviation <- data.frame(motifs = motifs)
-    for ( bedfile in files_list) {
-        sample_name = basename(bedfile)
-        log_info("Processing ", bedfile)
-        msites <- read_methylome(bedfile, type = filetype)
-        assign(sample_name, mclapply(motifs, compute_deviation, 
-                                    msites = msites, 
-                                    tf_bindsites = tf_bindsites, 
-                                    gcfreqs = gcfreqs, 
-                                    gcdist = gc_dist,
-                                    enhancer = enhancer ,
-                                    mc.cores = threads))  
-        log_info('Done ', bedfile)
-        #deviation[, ':='(sample_name = unlist(process_deviation))]
-        deviation[sample_name] = unlist(get(sample_name))
-    }
+  annfile <- file.path(sample_dir, sample_ann)
+  if (!file.exists(annfile)) {
+    log_error(" %s does not exist, please check the file path !!", annfile)
+  }
 
-    #rownames(deviation) <- motifs
-    
-    return(deviation)
+  samples <- read.table(annfile, sep = "\t", header = TRUE)
+  filetype <- match.arg(filetype)
+
+  logger::log_info("Loading the samples and annotation package ")
+  files_list <- file.path(sample_dir, samples[, "bedFile"])
+  tf_bindsites <- getTFbindsites()
+  gc_dist <- getGenomeGC()
+  gcfreqs <- getGCfreq()
+
+  motifs <- names(gcfreqs)
+  deviation <- data.frame(motifs = motifs)
+  for (bedfile in files_list) {
+    sample_name <- basename(bedfile)
+    logger::log_info("Processing ", bedfile)
+    msites <- read_methylome(bedfile, type = filetype)
+    assign(sample_name, mclapply(motifs, compute_deviation,
+      msites = msites,
+      tf_bindsites = tf_bindsites,
+      gcfreqs = gcfreqs,
+      gcdist = gc_dist,
+      enhancer = enhancer,
+      mc.cores = threads
+    ))
+    log_info("Finished processing ", bedfile)
+    # deviation[, ':='(sample_name = unlist(process_deviation))]
+    deviation[sample_name] <- unlist(get(sample_name))
+  }
+
+  # rownames(deviation) <- motifs
+
+  return(deviation)
 }
--- example/run_methylTFR.R
+++ example/run_methylTFR.R
@@ -1,4 +1,3 @@
-
 library(methylTFRann)
 library(methylTFR)
 
@@ -13,17 +12,17 @@ motif_gcfreq <- getGCfreq()
 motif_list <- names(motif_gcfreq)
 
 # files to be processed
-files_list <- list.files(path="/data/blueprint/bed", pattern='*.bed', full.names=T)
+files_list <- list.files(path = "/data/blueprint/bed", pattern = "*.bed", full.names = T)
 deviation_distal <- data.frame(motifs = names(motif_gcfreq))
 for (fname in files_list) {
-    basename = unlist(str_split(fname, "/"))[10]
-    prefix = str_replace(basename, ".bed", "")
-    if (! prefix %in% colnames(deviation_distal)){
-        print(paste0(fname, " Processing ..."))
-        msites <- read_methylome(fname)
-        assign(prefix, mclapply(motif_list, compute_deviation, msites = msites, tf_bindsites = tf_bindsites, gcfreqs = motif_gcfreq, gcdist = gc_dist, mc.cores = 16))
-        deviation_distal[prefix] = unlist(get(prefix))
-        print("Done")
-        save(deviation_distal, file = "/data/gc_corrected_distal_deviation_all.Rds")
-    }
+  basename <- unlist(str_split(fname, "/"))[10]
+  prefix <- str_replace(basename, ".bed", "")
+  if (!prefix %in% colnames(deviation_distal)) {
+    print(paste0(fname, " Processing ..."))
+    msites <- read_methylome(fname)
+    assign(prefix, mclapply(motif_list, compute_deviation, msites = msites, tf_bindsites = tf_bindsites, gcfreqs = motif_gcfreq, gcdist = gc_dist, mc.cores = 16))
+    deviation_distal[prefix] <- unlist(get(prefix))
+    print("Done")
+    save(deviation_distal, file = "/data/gc_corrected_distal_deviation_all.Rds")
+  }
 }
